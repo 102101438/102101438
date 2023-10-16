6f986faa18d8efb9ab72ec920a83cb4d5c3e7741
102101438 <145024243+102101438@users.noreply.github.com>
2023-10-16 23:46:15 +0800

    gzyz
    gzyz

---
+++ all in/pages/caidan/caidan.js
@@ -0,0 +1,12 @@
+Page({
+  tobendi: function() {
+    wx.navigateTo({
+      url: '../bendi/bendi'
+    })
+  },
+  toguize: function() {
+    wx.navigateTo({
+      url: '../guize/guize'
+    })
+  },
+})
